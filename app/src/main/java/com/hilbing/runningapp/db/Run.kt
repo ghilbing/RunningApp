8cda1f1eeaac1c2e064deb0c5545792813142230
ghilbing <ghilbing@gmail.com>
2021-04-13 12:44:31 -0700

    Adding Dagger Hilt

--- app/src/main/java/com/hilbing/runningapp/db/Run.kt
+++ app/src/main/java/com/hilbing/runningapp/db/Run.kt
@@ -9,7 +9,7 @@ import java.sql.Timestamp
 data class Run(
     var img: Bitmap? = null,
     var timestamp: Long = 0L,
-    var avgSpeedInKMH: Float = 0F,
+    var avgSpeedInKMH: Float = 0f,
     var distanceInMeters: Int = 0,
     var timeInMillis: Long = 0L,
     var caloriesBurned: Int = 0
